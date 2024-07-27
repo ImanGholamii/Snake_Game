eca3a56d56267804ad21807ac225d01bb4b19bff
Iman <imangholamiimi@gmail.com>
2024-07-27 21:22:21 +0330

    :zap: | UPDATE | added move functionality

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 from turtle import Turtle, Screen
+from time import sleep
+
+# Screen configs
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("Snake Game")
+screen.tracer(0)
 
 t_obj = Turtle()
 food = Turtle()
@@ -18,42 +26,28 @@ for i in range(3):
     snake.penup()
     snake_list.append(snake)
     snake.goto(start_positions[i])
-
-
-class MoveSnake:
-    def move(self):
-        pass
-
-
-class Forward(MoveSnake):
-    def __init__(self, turtle_obj):
-        self.turtle_obj = turtle_obj
-
-    def move(self):
-        """takes obj lists and move them forward"""
-        for obj in self.turtle_obj:
-            obj.forward(60)
-
-
-forward_move = Forward(snake_list)
-
-
-def move_fd():
-    forward_move.move()
+    food.shapesize(0.5)
 
 
 # Food
 food.shape('circle')
 food.color('red')
-food.shapesize(0.5)
-
-screen = Screen()
-screen.setup(width=600, height=600)
-print(screen.screensize())
-screen.bgcolor("black")
-screen.title("Snake Game")
-screen.onkey(fun=move_fd(), key="w")
+# # Keys
+# screen.onkey(fun=move_fd(), key="w")
 # screen.onkey(fun=backward, key="s")
 # screen.onkey(fun=go_to_right, key="d")
 # screen.onkey(fun=go_to_left, key="a")
+
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+    sleep(1)
+    for snake in range(len(snake_list) - 1, 0, -1):
+        new_x = snake_list[snake - 1].xcor()
+        new_y = snake_list[snake - 1].ycor()
+        print(snake-1, new_x, new_y)
+        snake_list[snake].goto(new_x, new_y)
+    snake_list[0].fd(20)
+
 screen.exitonclick()
