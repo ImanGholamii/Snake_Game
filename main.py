704e9352e4858d52eb37820549141710d0be51fa
Iman <imangholamiimi@gmail.com>
2024-07-30 11:40:18 +0330

    :zap: | UPDATE | added event listeners to move with Arrow keys

--- main.py
+++ main.py
@@ -19,24 +19,28 @@ t_obj.hideturtle()
 t_obj.teleport(x=0, y=260)
 t_obj.write(f"Score: {score}", align="center", font=("Courier", 20, 'bold'))
 
-
 # Food
 food.shape('circle')
 food.color('gold')
 food.penup()
 food.shapesize(0.5)
 food.goto((0, 50))
-# # Keys
-# screen.onkey(fun=move_fd(), key="w")
-# screen.onkey(fun=backward, key="s")
-# screen.onkey(fun=go_to_right, key="d")
-# screen.onkey(fun=go_to_left, key="a")
 
 snake = Snake()
+
+# # Keys
+screen.listen()
+screen.onkey(fun=snake.move_up, key="Up")
+screen.onkey(fun=snake.move_down, key="Down")
+screen.onkey(fun=snake.move_right, key="Right")
+screen.onkey(fun=snake.move_left, key="Left")
+
 game_is_on = True
 while game_is_on:
     screen.update()
-    sleep(1)
+    sleep(0.1)  # refresh the page every 1 seconds
+
+    snake.move()
 
 
 screen.exitonclick()
