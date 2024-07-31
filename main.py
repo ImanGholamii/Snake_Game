106bbd492ee5f8c1c410fa267e71ce1454f14d21
Iman <imangholamiimi@gmail.com>
2024-07-31 15:26:13 +0330

    :zap: | UPDATE | using from extend_snake method when get score

--- main.py
+++ main.py
@@ -35,6 +35,7 @@ while game_is_on:
     if snake.head.distance(food) <= 15:
         food.refresh()
         score.add_score()
+        snake.extend_snake()
 
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 282 or snake.head.ycor() < -300:
         game_is_on = False
