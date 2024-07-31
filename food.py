440c143a1889dd44edd4cf1779b4a8b9404982ea
Iman <imangholamiimi@gmail.com>
2024-07-31 13:07:18 +0330

    :sparkles: | ADD | added random cordinates to food location

--- food.py
+++ food.py
@@ -1,15 +1,19 @@
-from turtle import Turtle, Screen
+from turtle import Turtle
+from random import choice
 
 class Food(Turtle):
+    coordinates = []
+    for x in range(-280, 280, 2):
+        for y in range(-280, 260, 2):
+            coordinates.append((x, y))
+    random_cor = choice(coordinates)
+    print(random_cor)
+
     def __init__(self):
         super().__init__()
         self.penup()
         self.color("gold")
         self.shape("circle")
-        self.shapesize(0.5)
-
-
-food = Food()
-
-screen = Screen()
-screen.exitonclick()
+        self.shapesize(stretch_wid=0.49, stretch_len=0.49)
+        self.speed("fastest")
+        self.goto(self.random_cor)
