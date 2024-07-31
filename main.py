ba55b7a41bf8f723afeace50652d18226f7cc9f3
Iman <imangholamiimi@gmail.com>
2024-07-31 13:13:26 +0330

    :zap: | UPFATE | Fix window scale show

--- main.py
+++ main.py
@@ -1,25 +1,27 @@
 from turtle import Turtle, Screen
 from time import sleep
 from snake import Snake
+from food import Food
 
 # Screen configs
 screen = Screen()
-screen.setup(width=600, height=600)
+screen.setup(width=600, height=620)
 screen.bgcolor("black")
 screen.title("Snake Game")
 screen.tracer(0)
 
 t_obj = Turtle()
-food = Turtle()
 
 # background
 score = 0
 t_obj.color("white")
 t_obj.hideturtle()
-t_obj.teleport(x=0, y=260)
+t_obj.teleport(x=0, y=283)
 t_obj.write(f"Score: {score}", align="center", font=("Courier", 20, 'bold'))
 
+# imported module objects
 snake = Snake()
+food = Food()
 
 # # Keys
 screen.listen()
