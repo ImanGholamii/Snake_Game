f3659664ce6f2e5ca98792a53d54faeac00e55fc
Iman <imangholamiimi@gmail.com>
2024-08-12 13:52:25 +0330

    :zap: | UPDATE | added high_score to update_score, Removed game_over method | :sprkles: | ADD | added reset method

--- scoreboard.py
+++ scoreboard.py
@@ -15,12 +15,13 @@ class Score(Turtle):
         self.update_score()
 
     def update_score(self):
-        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+        self.write(f"Score: {self.score} High Score: {self.high_score}", align=ALIGNMENT, font=FONT)
 
-    def game_over(self):
-        self.teleport(x=0, y=0)
-        self.color("light pink")
-        self.write(f"GAME OVER", align=ALIGNMENT, font=FONT)
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+        self.score = 0
+        self.update_score()
 
     def add_score(self):
         self.score += 1
