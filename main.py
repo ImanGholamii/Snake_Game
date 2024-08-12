2c67b138f78abb746911609dadd771d37c0cd08d
Iman <imangholamiimi@gmail.com>
2024-08-12 18:23:56 +0330

    :zap: | UPDATE | used from snake.reset method in line 42, 47

--- main.py
+++ main.py
@@ -39,9 +39,11 @@ while game_is_on:
 
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 282 or snake.head.ycor() < -300:
         score.reset()
+        snake.reset()
 
     for segment in snake.snake_segments[1:]:
         if snake.head.distance(segment) < 10:
             score.reset()
+            snake.reset()
 
 screen.exitonclick()
